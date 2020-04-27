8b21177244ab384b7b8584aaece5890f1594fca3
Alex Sims <alexander.sims92@gmail.com>
2020-04-27 14:34:56 +0100

    Added missing calls to apply index

--- lib/Doctrine/Query/Abstract.php
+++ lib/Doctrine/Query/Abstract.php
@@ -2503,8 +2503,7 @@ abstract class Doctrine_Query_Abstract
                 }
             }
         }
-        // Empty the index part array to prevent us from accidentally appending twice.
-        $this->_index_parts = [];
+
         return $sql;
     }
 
