c2c2440e36cdb2fa5da0216bedd02df0d0b650c5
Tybaze <tybaze@users.noreply.github.com>
2022-08-02 17:19:50 +0200

    Handle default taskName to null, to avoid triggering the strlen(null) php 8.1 incompatibility

--- lib/Doctrine/Task.php
+++ lib/Doctrine/Task.php
@@ -56,7 +56,7 @@ abstract class Doctrine_Task
         $taskName = (string) $this->getTaskName();
 
         //Derive the task name only if it wasn't entered at design-time
-        if (! strlen($taskName)) {
+        if (null === $taskName || ! strlen($taskName)) {
             $taskName = self::deriveTaskName(get_class($this));
         }
 
